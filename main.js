03fdc4d559cebe7c2afba132546321f5977bb90d
null <null>
2023-11-29 20:06:51 -0300

    cria lista.js e main.js

---
+++ main.js
@@ -0,0 +1,14 @@
+//main.js
+import { getLista } from "./lista.js";
+
+//Passo 1 - Ache os elementos importantes
+const pEntrada = document.querySelector('#entrada');
+const olSaida = document.querySelector('#saida');
+const btnAdicionar = document.querySelector('#adicionar');
+const btnLimpar = document.querySelector('#limpar');
+
+olSaida.innerHTML = "";
+
+function atualizarItensDeLista() {
+    
+}
